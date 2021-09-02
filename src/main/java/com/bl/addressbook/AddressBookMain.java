e7b1f8985a0f24b143a0399c77824a53c5d0b13a
jumdemonali <jumdemonali@gmail.com>
2021-09-02 20:44:36 +0530

    Added: Contacts in address book

--- src/main/java/com/bl/addressbook/AddressBookMain.java
+++ src/main/java/com/bl/addressbook/AddressBookMain.java
@@ -1,7 +1,33 @@
 package com.bl.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    String firstName, lastName, address, city, state, email;
+    int zip;
+    long phoneNo;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        addContact();
+    }
+
+    public static void addContact() {
+        System.out.println("Enter the contact details:");
+        System.out.println("Enter first name:");
+        String firstName = sc.nextLine();
+        System.out.println("Enter last name");
+        String lastName = sc.nextLine();
+        System.out.println("Enter city");
+        String city = sc.nextLine();
+        System.out.println("Enter state");
+        String state = sc.nextLine();
+        System.out.println("Enter Zip");
+        String zip = sc.nextLine();
+        System.out.println("Enter Phone");
+        String phoneNumber = sc.nextLine();
+        System.out.println("Enter email");
+        String email = sc.nextLine();
     }
 }
