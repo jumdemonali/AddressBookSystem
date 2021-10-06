727c1197f875147da9815384c42645a1e604ee7c
jumdemonali <jumdemonali@gmail.com>
2021-10-06 13:10:27 +0530

    Added: Ability to retrieve data based on paticular date

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -11,12 +11,19 @@ public class AddressBook {
         AddressBook addressBook=new AddressBook();
         addressBook.retrieveData();
         addressBook.updateSalary();
+        addressBook.getContactsInDatePeriod();
     }
-        private void retrieveData () throws SQLException {
+
+    private void getContactsInDatePeriod() throws SQLException {
+        addressBookRepo.getContactsInDatePeriod();
+    }
+
+    private void retrieveData () throws SQLException {
             List<Person> employeeInfoList = addressBookRepo.retrieveData();
             System.out.println(employeeInfoList);
         }
     private void updateSalary() {
         addressBookRepo.updateSalary("Mona",5000);
     }
+
     }
